d6b8a9275ec00166b7facd63751ddb5a395e7aa0
shofiya <shofiyabootwala@gmail.com>
2025-03-20 06:50:58 +0530

    refactor: added print statements and checked for nil value of list parts output

--- utils/upload.go
+++ utils/upload.go
@@ -11,7 +11,6 @@ import (
 )
 
 func ListMultipartUploadParts(bucket, key, uploadId string) ([]types.CompletedPart, error) {
-
 	cloudSession, err := config.GetSession()
 
 	if err != nil {
@@ -19,25 +18,32 @@ func ListMultipartUploadParts(bucket, key, uploadId string) ([]types.CompletedPa
 	}
 
 	s3Client := cloudSession.AWS
+	fmt.Printf("Listing parts for bucket: %s, key: %s, uploadId: %s\n", bucket, key, uploadId)
+
 	listPartsOutput, err := s3Client.ListParts(context.TODO(), &s3.ListPartsInput{
 		Bucket:   aws.String(bucket),
 		Key:      aws.String(key),
 		UploadId: aws.String(uploadId),
 	})
 
-	var completedParts []types.CompletedPart
+	if err != nil {
+		return nil, fmt.Errorf("failed to list parts: %v", err)
+	}
 
+	if listPartsOutput == nil {
+		return nil, fmt.Errorf("list parts output is nil")
+	}
+
+	fmt.Printf("Found %d parts in the multipart upload\n", len(listPartsOutput.Parts))
+
+	var completedParts []types.CompletedPart
 	for _, part := range listPartsOutput.Parts {
+		fmt.Printf("Part %d: ETag %s\n", part.PartNumber, *part.ETag)
 		completedParts = append(completedParts, types.CompletedPart{
 			ETag:       part.ETag,
 			PartNumber: part.PartNumber,
 		})
 	}
 
-	if err != nil {
-		return nil, fmt.Errorf("failed to list parts: %v", err)
-	}
-
 	return completedParts, nil
-
 }
